4c27693b9529fe09df8c30d6daa4b751852491f8
sebrraj-sg <seb.rraj@gmail.com>
2026-08-16 03:17:03 +0800

    3

--- pages/api/auth/[...all].js
+++ pages/api/auth/[...all].js
@@ -1,8 +1,4 @@
 import { auth } from "@/lib/auth";
 import { toNodeHandler } from "better-auth/node";
 
-const handler = toNodeHandler(auth);
-
-export default async function authHandler(req, res) {
-  return handler(req, res);
-}
+export default toNodeHandler(auth);
